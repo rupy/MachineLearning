d32e6a4e4138942bae3defd724ee858602ae5c96
Rupy <rupyapps@gmail.com>
2014-06-24 00:33:12 +0900

    タブをスペースに変換

--- nearest_neighbor_rule.rb
+++ nearest_neighbor_rule.rb
@@ -3,23 +3,23 @@
 #
 class Point
 
-	# 2次元上の座標
-	attr_accessor :x, :y
+  # 2次元上の座標
+  attr_accessor :x, :y
 
-	# 座標を初期化
-	def initialize(x, y)
-		@x, @y = x, y
-	end
+  # 座標を初期化
+  def initialize(x, y)
+    @x, @y = x, y
+  end
 
-	# 座標を表示
-	def print
-		puts "(#{@x}, #{@y})"
-	end
+  # 座標を表示
+  def print
+    puts "(#{@x}, #{@y})"
+  end
 
-	# 他の点を引数に受け取り，その点との距離を計算する
-	def dist(p)
-		Math.sqrt( (@x - p.x) ** 2 + (@y - p.y) ** 2)
-	end
+  # 他の点を引数に受け取り，その点との距離を計算する
+  def dist(p)
+    Math.sqrt( (@x - p.x) ** 2 + (@y - p.y) ** 2)
+  end
 end
 
 #
@@ -27,46 +27,46 @@ end
 #
 class NNRule
 
-	# 初期化
-	def initialize
-		@points = Hash.new
-	end
+  # 初期化
+  def initialize
+    @points = Hash.new
+  end
 
-	# 点を追加
-	def add_point(p, class_name)
-		unless @points.has_key? class_name
-			@points[class_name] = []
-		end
-		@points[class_name].push p
-	end
+  # 点を追加
+  def add_point(p, class_name)
+    unless @points.has_key? class_name
+      @points[class_name] = []
+    end
+    @points[class_name].push p
+  end
 
-	# 点をすべて表示
-	def print_all_points(class_name)
-		if @points.has_key? class_name
-			@points[class_name].each do |p|
-				p.print
-			end
-		end
-	end
+  # 点をすべて表示
+  def print_all_points(class_name)
+    if @points.has_key? class_name
+      @points[class_name].each do |p|
+        p.print
+      end
+    end
+  end
 
-	# 点pと一番近い点を出力
-	def nearest_neighbor(p)
-		min = Float::INFINITY
-		min_p = nil 
-		min_c = nil
-		@points.each do |c, class_points|
-			class_points.each do |p2|
-				d = p.dist p2
-				if d < min
-					min = d
-					min_p = p2
-					min_c = c
-				end
-			end
-		end
-		puts "class: #{min_c}"
-		min_p.print
-	end
+  # 点pと一番近い点を出力
+  def nearest_neighbor(p)
+    min = Float::INFINITY
+    min_p = nil 
+    min_c = nil
+    @points.each do |c, class_points|
+      class_points.each do |p2|
+        d = p.dist p2
+        if d < min
+          min = d
+          min_p = p2
+          min_c = c
+        end
+      end
+    end
+    puts "class: #{min_c}"
+    min_p.print
+  end
 
 end
 
